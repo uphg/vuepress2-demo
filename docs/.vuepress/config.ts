310550e9e4004ee0fa3019c4b5c233f5876fee24
ChenNing02 <2633565885@qq.com>
2021-10-21 14:15:08 +0800

    测试使用插件导入临时文件的方法

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,16 @@
-import p from 'path'
-import { fooPlugin } from '../plugins/foo'
+import path from 'path'
+const fooPlugin = require('../plugins/foo.ts') 
 
 export default {
+  // theme: themeConfig => {
+  //   return {
+  //     layouts: {
+  //       Layout: path.resolve(__dirname, 'docs/.vuepress/theme/layouts/Layout.vue'),
+  //       404: path.resolve(__dirname, 'docs/.vuepress/theme/layouts/404.vue'),
+  //       FooBar: path.resolve(__dirname, 'docs/.vuepress/theme/layouts/FooBar.vue'),
+  //     },
+  //   }
+  // },
   plugins: [
     [
       fooPlugin,
